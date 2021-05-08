ef946f2a7ba1f21f07ad1e6c0e21adc97ffa2419
Mason Xiong <mason.xiong@yahoo.com>
2021-05-08 12:11:07 +1000

    3-2 package: morgan

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
 const routes = require('./routes');
 const app = express();
 
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors());
 

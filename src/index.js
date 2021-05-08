5f615bd0e0e3249f433bbb780dc4c809e9aaafd3
Mason Xiong <mason.xiong@yahoo.com>
2021-05-08 12:09:40 +1000

    3-1 package: cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const cors = require('./middleware/cors');
+const cors = require('cors');
 const routes = require('./routes');
 const app = express();
 
 app.use(express.json());
-app.use(cors);
+app.use(cors());
 
 app.use(routes);
 
